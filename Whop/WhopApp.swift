8f0ee968fa24438c334daa2cb90f68d2c484458e
Eric Chang <echang25@hotmail.com>
2024-06-03 16:29:30 -0400

    Initial Commit

---
+++ Whop/WhopApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhopApp.swift
+//  Whop
+//
+//  Created by Eric Chang on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct WhopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
